7040b2cba62706e3f68ea42aef3c1df5364915d0
Nils DEFAUW <nils@Debian-MacBookProDeNils>
2017-12-20 15:14:03 +0100

    Nouvelle implémentation de registers.c et registers_test.c

--- registers.c
+++ registers.c
@@ -24,57 +24,166 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_constants.h"
 #include <stdlib.h>
 
+#define NB_PHYS_REGS (37)
+#define NB_VIRT_REGS (18)
+#define NB_UNBANKED_REGS (8)
+#define NB_2BANKED_REGS (5)
+#define NB_6BANKED_REGS (2)
+#define NB_MODES (7)
+
 struct registers_data {
+	uint8_t mode;
+	uint32_t registers[NB_PHYS_REGS];
+	uint32_t *virtual_regs[NB_MODES*NB_VIRT_REGS];
 };
 
+int get_mode_num(registers r)
+{
+	switch (r->mode)
+	{
+		case USR:
+			return 0;
+		case FIQ:
+			return 1;
+		case IRQ:
+			return 2;
+		case SVC:
+			return 3;
+		case ABT:
+			return 4;
+		case UND:
+			return 5;
+		default:
+			return 6;
+	}
+}
+
 registers registers_create() {
-    registers r = NULL;
+    registers r = malloc(sizeof(struct registers_data));
+	r->mode = SYS;
+	//USR
+	for (int i = 0; i <= 7; i++)
+		r->virtual_regs[0*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 8; i <= 12; i++)
+		r->virtual_regs[0*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 13; i <= 14; i++)
+		r->virtual_regs[0*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS]);
+	r->virtual_regs[0*NB_VIRT_REGS+15] = &(r->registers[15+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[0*NB_VIRT_REGS+16] = &(r->registers[16+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[0*NB_VIRT_REGS+17] = NULL;
+	//FIQ
+	for (int i = 0; i <= 7; i++)
+		r->virtual_regs[1*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 8; i <= 12; i++)
+		r->virtual_regs[1*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS]);
+	for (int i = 13; i <= 14; i++)
+		r->virtual_regs[1*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS+NB_6BANKED_REGS]);
+	r->virtual_regs[1*NB_VIRT_REGS+15] = &(r->registers[15+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[1*NB_VIRT_REGS+16] = &(r->registers[16+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[1*NB_VIRT_REGS+17] = &(r->registers[17+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	//IRQ
+	for (int i = 0; i <= 7; i++)
+		r->virtual_regs[2*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 8; i <= 12; i++)
+		r->virtual_regs[2*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 13; i <= 14; i++)
+		r->virtual_regs[2*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS+2*NB_6BANKED_REGS]);
+	r->virtual_regs[2*NB_VIRT_REGS+15] = &(r->registers[15+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[2*NB_VIRT_REGS+16] = &(r->registers[16+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[2*NB_VIRT_REGS+17] = &(r->registers[17+NB_2BANKED_REGS+5*NB_6BANKED_REGS+1]);
+	//SVC
+	for (int i = 0; i <= 7; i++)
+		r->virtual_regs[3*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 8; i <= 12; i++)
+		r->virtual_regs[3*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 13; i <= 14; i++)
+		r->virtual_regs[3*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS+3*NB_6BANKED_REGS]);
+	r->virtual_regs[3*NB_VIRT_REGS+15] = &(r->registers[15+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[3*NB_VIRT_REGS+16] = &(r->registers[16+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[3*NB_VIRT_REGS+17] = &(r->registers[17+NB_2BANKED_REGS+5*NB_6BANKED_REGS+2]);
+	//ABT
+	for (int i = 0; i <= 7; i++)
+		r->virtual_regs[4*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 8; i <= 12; i++)
+		r->virtual_regs[4*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 13; i <= 14; i++)
+		r->virtual_regs[4*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS+4*NB_6BANKED_REGS]);
+	r->virtual_regs[4*NB_VIRT_REGS+15] = &(r->registers[15+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[4*NB_VIRT_REGS+16] = &(r->registers[16+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[4*NB_VIRT_REGS+17] = &(r->registers[17+NB_2BANKED_REGS+5*NB_6BANKED_REGS+3]);
+	//UND
+	for (int i = 0; i <= 7; i++)
+		r->virtual_regs[5*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 8; i <= 12; i++)
+		r->virtual_regs[5*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 13; i <= 14; i++)
+		r->virtual_regs[5*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[5*NB_VIRT_REGS+15] = &(r->registers[15+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[5*NB_VIRT_REGS+16] = &(r->registers[16+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[5*NB_VIRT_REGS+17] = &(r->registers[17+NB_2BANKED_REGS+5*NB_6BANKED_REGS+4]);
+	//SYS
+	for (int i = 0; i <= 7; i++)
+		r->virtual_regs[6*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 8; i <= 12; i++)
+		r->virtual_regs[6*NB_VIRT_REGS+i] = &(r->registers[i]);
+	for (int i = 13; i <= 14; i++)
+		r->virtual_regs[6*NB_VIRT_REGS+i] = &(r->registers[i+NB_2BANKED_REGS]);
+	r->virtual_regs[6*NB_VIRT_REGS+15] = &(r->registers[15+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[6*NB_VIRT_REGS+16] = &(r->registers[16+NB_2BANKED_REGS+5*NB_6BANKED_REGS]);
+	r->virtual_regs[6*NB_VIRT_REGS+17] = NULL;
+	for (int i = 0; i < NB_PHYS_REGS; i++)
+		r->registers[i] = 0;
     return r;
 }
 
 void registers_destroy(registers r) {
+	free(r);
 }
 
 uint8_t get_mode(registers r) {
-    return 0;
+    return r->mode;
 } 
 
 int current_mode_has_spsr(registers r) {
-    return 0;
+    return (r->virtual_regs[get_mode_num(r)*NB_VIRT_REGS+17] != NULL);
 }
 
 int in_a_privileged_mode(registers r) {
-    return 0;
+    return (get_mode_num(r) != 0);
 }
 
 uint32_t read_register(registers r, uint8_t reg) {
-    uint32_t value=0;
+    uint32_t value = *(r->virtual_regs[get_mode_num(r)*NB_VIRT_REGS+reg]);
     return value;
 }
 
 uint32_t read_usr_register(registers r, uint8_t reg) {
-    uint32_t value=0;
+    uint32_t value = *(r->virtual_regs[reg]);
     return value;
 }
 
 uint32_t read_cpsr(registers r) {
-    uint32_t value=0;
+    uint32_t value = *(r->virtual_regs[get_mode_num(r)*NB_VIRT_REGS+16]);
     return value;
 }
 
 uint32_t read_spsr(registers r) {
-    uint32_t value=0;
+    uint32_t value = *(r->virtual_regs[get_mode_num(r)*NB_VIRT_REGS+17]);
     return value;
 }
 
 void write_register(registers r, uint8_t reg, uint32_t value) {
+	*(r->virtual_regs[get_mode_num(r)*NB_VIRT_REGS+reg]) = value;
 }
 
 void write_usr_register(registers r, uint8_t reg, uint32_t value) {
+	*(r->virtual_regs[reg]) = value;
 }
 
 void write_cpsr(registers r, uint32_t value) {
+	*(r->virtual_regs[get_mode_num(r)*NB_VIRT_REGS+16]) = value;
 }
 
 void write_spsr(registers r, uint32_t value) {
+	*(r->virtual_regs[get_mode_num(r)*NB_VIRT_REGS+17]) = value;
 }
