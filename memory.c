1c2eeaff460a9b68479e4b367ee8e575848a17ee
Nils DEFAUW <nils@Debian-MacBookProDeNils>
2017-12-20 20:58:37 +0100

    memory.c réimplémenté plus proprement

--- memory.c
+++ memory.c
@@ -25,40 +25,78 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 
 struct memory_data {
+	size_t size;
+	int is_big_endian;
+	uint8_t *memory;
 };
 
 memory memory_create(size_t size, int is_big_endian) {
-    memory mem=NULL;
+    memory mem = malloc(sizeof(struct memory_data));
+	mem->size = size;
+	mem->is_big_endian = is_big_endian;
+	mem->memory = malloc(size*sizeof(uint8_t));
     return mem;
 }
 
 size_t memory_get_size(memory mem) {
-    return 0;
+    return mem->size;
 }
 
 void memory_destroy(memory mem) {
+	free(mem->memory);
+	free(mem);
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
-    return -1;
+	if (address >= mem->size)
+		return -1;
+    *value = mem->memory[address];
+	return 0;
 }
 
 int memory_read_half(memory mem, uint32_t address, uint16_t *value) {
-    return -1;
+	if (address >= mem->size-1)
+		return -1;
+	if (is_big_endian() == mem->is_big_endian)
+		*value = *((uint16_t *)(&(mem->memory[address])));
+	else
+		*value = reverse_2(*((uint16_t *)(&(mem->memory[address]))));
+    return 0;
 }
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
-    return -1;
+	if (address >= mem->size-3)
+		return -1;
+	if (is_big_endian() == mem->is_big_endian)
+		*value = *((uint32_t *)(&(mem->memory[address])));
+	else
+		*value = reverse_4(*((uint32_t *)(&(mem->memory[address]))));
+    return 0;
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
-    return -1;
+	if (address >= mem->size)
+		return -1;
+	mem->memory[address] = value;
+    return 0;
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value) {
-    return -1;
+	if (address >= mem->size-1)
+		return -1;
+	if (is_big_endian() == mem->is_big_endian)
+		*((uint16_t *)(&(mem->memory[address]))) = value;
+	else
+		*((uint16_t *)(&(mem->memory[address]))) = reverse_2(value);
+    return 0;
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value) {
-    return -1;
+	if (address >= mem->size-3)
+		return -1;
+	if (is_big_endian() == mem->is_big_endian)
+		*((uint32_t *)(&(mem->memory[address]))) = value;
+	else
+		*((uint32_t *)(&(mem->memory[address]))) = reverse_4(value);
+    return 0;
 }
