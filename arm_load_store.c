994ee80d63b84b04da9ccadbf0fa805a88bbf5b4
Nils DEFAUW <nils@Debian-MacBookProDeNils>
2017-12-21 20:17:08 +0100

    arm_load_store.c terminé

--- arm_load_store.c
+++ arm_load_store.c
@@ -30,6 +30,11 @@ int str(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, 
 int strb(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, int w_bit);
 int ldr(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, int w_bit);
 int ldrb(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, int w_bit);
+int strh(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, int w_bit);
+int ldrh(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, int w_bit);
+
+int ldm(arm_core p, uint32_t ins);
+int stm(arm_core p, uint32_t ins);
 
 uint32_t decode_off(arm_core p, uint32_t ins)
 {
@@ -65,15 +70,24 @@ uint32_t decode_off(arm_core p, uint32_t ins)
 	}
 }
 
+uint32_t decode_off_h(arm_core p, uint32_t ins)
+{
+	if (!get_bit(ins, 22))
+	{
+		return (get_bits(ins, 11, 8) << 4) | get_bits(ins, 3, 0);
+	}
+	return arm_read_register(p, get_bits(ins, 3, 0));
+}
+
 int arm_load_store(arm_core p, uint32_t ins) {
+	uint8_t rn = get_bits(ins, 19, 16);
+	uint8_t rd = get_bits(ins, 15, 12);
+	int p_bit = get_bit(ins, 24);
+	int u_bit = get_bit(ins, 23);
+	int w_bit = get_bit(ins, 21);
 	if (get_bit(ins, 26))
 	{
 		uint32_t off = decode_off(p, ins);
-		uint8_t rn = get_bits(ins, 19, 16);
-		uint8_t rd = get_bits(ins, 15, 12);
-		int p_bit = get_bit(ins, 24);
-		int u_bit = get_bit(ins, 23);
-		int w_bit = get_bit(ins, 21);
 		switch (get_bit(ins, 22) << 1 | get_bit(ins, 20))
 		{
 			case 0b00:
@@ -88,12 +102,17 @@ int arm_load_store(arm_core p, uint32_t ins) {
 	}
 	else
 	{
-		return UNDEFINED_INSTRUCTION;	//Rajouter ldrh et strh
+		uint32_t off = decode_off_h(p, ins);
+		if (get_bit(ins, 20))
+			return ldrh(p, rd, rn, off, p_bit, u_bit, w_bit);
+		return strh(p, rd, rn, off, p_bit, u_bit, w_bit);
 	}
 }
 
 int arm_load_store_multiple(arm_core p, uint32_t ins) {
-    return UNDEFINED_INSTRUCTION;
+    if (get_bit(ins, 20))
+		return ldm(p, ins);
+	return stm(p, ins);
 }
 
 int arm_coprocessor_load_store(arm_core p, uint32_t ins) {
@@ -264,5 +283,281 @@ int ldrb(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit,
 	if (result)
 		return result;
 	arm_write_register(p, rd, (uint32_t)rd_cont);
+	return result;
+}
+
+int strh(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, int w_bit)
+{
+	uint32_t rn_cont = arm_read_register(p, rn);
+	uint16_t rd_cont = (uint16_t)(arm_read_register(p, rd));
+	uint32_t address;
+	int result;
+	if (u_bit)
+	{
+		address = rn_cont + off;
+		if (p_bit)
+		{
+			result = arm_write_half(p, address, rd_cont);
+			if (w_bit)
+				arm_write_register(p, rn, address);
+		}
+		else
+		{
+			result = arm_write_half(p, rn_cont, rd_cont);
+			arm_write_register(p, rn, address);
+		}
+	}
+	else 
+	{
+		address = rn_cont - off;
+		if (p_bit)
+		{
+			result = arm_write_half(p, address, rd_cont);
+			if (w_bit)
+				arm_write_register(p, rn, address);
+		}
+		else
+		{
+			result = arm_write_half(p, rn_cont, rd_cont);
+			arm_write_register(p, rn, address);
+		}
+	}
+	return result;
+}
+
+int ldrh(arm_core p, uint8_t rd, uint8_t rn, uint32_t off, int p_bit, int u_bit, int w_bit)
+{
+	uint32_t rn_cont = arm_read_register(p, rn);
+	uint16_t rd_cont;
+	uint32_t address;
+	int result;
+	if (u_bit)
+	{
+		address = rn_cont + off;
+		if (p_bit)
+		{
+			result = arm_read_half(p, address, &rd_cont);
+			if (w_bit)
+				arm_write_register(p, rn, address);
+		}
+		else
+		{
+			result = arm_read_half(p, rn_cont, &rd_cont);
+			arm_write_register(p, rn, address);
+		}
+	}
+	else 
+	{
+		address = rn_cont - off;
+		if (p_bit)
+		{
+			result = arm_read_half(p, address, &rd_cont);
+			if (w_bit)
+				arm_write_register(p, rn, address);
+		}
+		else
+		{
+			result = arm_read_half(p, rn_cont, &rd_cont);
+			arm_write_register(p, rn, address);
+		}
+	}
+	if (result)
+		return result;
+	arm_write_register(p, rd, (uint32_t)rd_cont);
+	return result;
+}
+
+int ldm(arm_core p, uint32_t ins)
+{
+	uint32_t address = arm_read_register(p, get_bits(ins, 19, 16));
+	uint32_t value;
+	int result = 0;
+	if (get_bit(ins, 23))
+	{
+		if (get_bit(ins, 24))
+			address += 4;
+		if (get_bit(ins, 22) && get_bit(ins, 15))
+			arm_write_cpsr(p, arm_read_spsr(p));
+		if (get_bit(ins, 22) && !get_bit(ins, 15))
+		{
+			for (int i = 0; i < 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					result |= arm_read_word(p, address, &value);
+					arm_write_usr_register(p, i, value);
+					address += 4;
+				}
+			}
+			if (get_bit(ins, 15))
+			{
+				result |= arm_read_word(p, address, &value);
+				arm_write_usr_register(p, 15, value & 0xFFFFFFFC);
+				address += 4;
+			}
+				
+		}
+		if (!get_bit(ins, 22))
+		{
+			for (int i = 0; i < 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					result |= arm_read_word(p, address, &value);
+					arm_write_register(p, i, value);
+					address += 4;
+				}
+			}
+			if (get_bit(ins, 15))
+			{
+				result |= arm_read_word(p, address, &value);
+				arm_write_register(p, 15, value & 0xFFFFFFFC);
+				address += 4;
+			}
+		}
+	}
+	else
+	{
+		if (get_bit(ins, 24))
+			address -= 4;
+		if (get_bit(ins, 22) && get_bit(ins, 15))
+			arm_write_cpsr(p, arm_read_spsr(p));
+		if (get_bit(ins, 22) && !get_bit(ins, 15))
+		{
+			for (int i = 0; i < 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					result |= arm_read_word(p, address, &value);
+					arm_write_usr_register(p, i, value);
+					address -= 4;
+				}
+			}
+			if (get_bit(ins, 15))
+			{
+				result |= arm_read_word(p, address, &value);
+				arm_write_usr_register(p, 15, value & 0xFFFFFFFC);
+				address -= 4;
+			}
+		}
+		if (!get_bit(ins, 22))
+		{
+			for (int i = 0; i < 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					result |= arm_read_word(p, address, &value);
+					arm_write_register(p, i, value);
+					address -= 4;
+				}
+			}
+			if (get_bit(ins, 15))
+			{
+				result |= arm_read_word(p, address, &value);
+				arm_write_register(p, 15, value & 0xFFFFFFFC);
+				address -= 4;
+			}
+		}
+	}
+	if (get_bit(ins, 21))
+	{
+		switch (get_bits(ins, 24, 23))
+		{
+			case 0b00:
+				arm_write_register(p, get_bits(ins, 19, 16), address);
+				break;
+			case 0b01:
+				arm_write_register(p, get_bits(ins, 19, 16), address);
+				break;
+			case 0b10:
+				arm_write_register(p, get_bits(ins, 19, 16), address + 4);
+				break;
+			default:
+				arm_write_register(p, get_bits(ins, 19, 16), address - 4);
+		}
+	}
+	return result;
+}
+
+int stm(arm_core p, uint32_t ins)
+{
+	uint32_t address = arm_read_register(p, get_bits(ins, 19, 16));
+	uint32_t value;
+	int result = 0;
+	if (get_bit(ins, 23))
+	{
+		if (get_bit(ins, 24))
+			address += 4;
+		if (get_bit(ins, 22))
+		{
+			for (int i = 0; i <= 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					value = arm_read_usr_register(p, i);
+					result |= arm_write_word(p, address, value);
+					address += 4;
+				}
+			}	
+		}
+		else
+		{
+			for (int i = 0; i <= 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					value = arm_read_register(p, i);
+					result |= arm_write_word(p, address, value);
+					address += 4;
+				}
+			}
+		}
+	}
+	else
+	{
+		if (get_bit(ins, 24))
+			address -= 4;
+		if (get_bit(ins, 22))
+		{
+			for (int i = 0; i <= 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					value = arm_read_usr_register(p, i);
+					result |= arm_write_word(p, address, value);
+					address -= 4;
+				}
+			}
+		}
+		else
+		{
+			for (int i = 0; i <= 15; i++)
+			{
+				if (get_bit(ins, i))
+				{
+					value = arm_read_register(p, i);
+					result |= arm_write_word(p, address, value);
+					address -= 4;
+				}
+			}
+		}
+	}
+	if (get_bit(ins, 21))
+	{
+		switch (get_bits(ins, 24, 23))
+		{
+			case 0b00:
+				arm_write_register(p, get_bits(ins, 19, 16), address);
+				break;
+			case 0b01:
+				arm_write_register(p, get_bits(ins, 19, 16), address);
+				break;
+			case 0b10:
+				arm_write_register(p, get_bits(ins, 19, 16), address + 4);
+				break;
+			default:
+				arm_write_register(p, get_bits(ins, 19, 16), address - 4);
+		}
+	}
 	return result;
 }
