0c548943ea96e5b3290f9e93b3ac76b02c48c81f
Neylix <julien.leclerc.05@hotmail.fr>
2018-01-11 21:37:39 +0100

    mise a jour script test (se lance sur le bon terminal)

--- Examples/test.sh
+++ Examples/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ../arm_simulator --gdb-port 41300 &
-konsole -e "arm-none-eabi-gdb $1 -x test_file.txt"
+term_pid=$(ps -o ppid= $PPID)
+term_cmd=$(ps $term_pid | grep / | cut -d/ -f 2-)
+/$term_cmd -e "arm-none-eabi-gdb $1 -x test_file.txt"
